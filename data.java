4f610a9e64354d52b5f87c3071d1637fdafa017d
sunil-95 <sunilaletii95@gmail.com>
2026-02-07 11:02:09 +0530

    Add files via upload

---
+++ data.java
@@ -0,0 +1,21 @@
+public class data {
+    public static void main(String[] args)
+    {
+        byte b=120;
+        short a=40;
+        int i=2;
+        long l=12345678;
+        float f=3.6f;
+        double d=3.5566;
+        char ch='A';
+        boolean bl=true;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(bl);
+    }
+}
