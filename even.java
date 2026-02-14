c357bbb38a3bf0c28211c6a9d4d7c39e16038835
sunil-95 <sunilaletii95@gmail.com>
2026-02-14 16:11:37 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,10 @@
+public class even{
+    public static void main(String[]args)
+    {
+        int i;
+        for(i=0;i<=50;i++)
+            if (i%2==0)
+            System.out.print(i+" ");
+    }
+}
+
