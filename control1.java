2005d3c5d0f9632af87ce91dab3b0cdf4abc196f
sunil-95 <sunilaletii95@gmail.com>
2026-02-14 12:11:43 +0530

    Add files via upload

---
+++ control1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class control1{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==0)System.out.println("Even");
+            else System.out.println("odd");
+    }
+}
